3e2a9ac367c928c46f88b65bfd209fedf3950e92
Abhay Kumar <abhay4waytechnologies@gmail.com>
2023-07-18 08:23:23 +0530

    javascript

--- 16july23/script.js
+++ 16july23/script.js
@@ -37,3 +37,36 @@ function isPrime(n) {
 }
 
 console.log("first999", isPrime(17))
+
+
+function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+  
+  // Example usage
+  console.log(factorial(5)); // Output: 120
+
+  
+//   The factorial function:
+
+function factorial(n){
+    return (n != 1)? n* factorial(n-1):1
+}
+
+console.log("60", factorial(5))
+
+
+
+function fib(n){
+    if(n<=1){
+        return n
+    } else{
+        return fib(n-1) + fib(n-2)
+    }
+}
+
+console.log("72", fib(10))
